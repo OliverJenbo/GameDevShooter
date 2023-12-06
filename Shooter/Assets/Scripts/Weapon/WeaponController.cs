91e51d2424a12ef474f2896923f134c9cad6f426
Oliver Jenbo <cph-oj92@cphbusiness.dk>
2023-12-06 05:37:17 +0100

    trying again

---
+++ Shooter/Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public void Shoot()
+    {
+        // Add shooting logic here
+        Debug.Log("Bang! Bang!");
+    }
+}
+
