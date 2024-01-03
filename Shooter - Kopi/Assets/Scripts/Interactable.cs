dbc290bb215f569c16d93d8124bacc241db8a3e9
Oliver Jenbo <cph-oj92@cphbusiness.dk>
2024-01-03 04:31:13 +0100

    trying again

---
+++ Shooter - Kopi/Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    //message displayed to player when looking at an interactable object.
+    public string promptmessage;
+
+    //this function will be called from the player
+    public void BaseInteract()
+    {
+        Interact();
+    }
+    protected virtual void Interact()
+    {
+        //template function to be overwritten in subclasses.
+    }
+}
